7a093a2d92a6d49a782a97e60bca8a45ccea80b9
Jixin Chen <40338321+nkchenjx@users.noreply.github.com>
2020-01-22 12:32:49 -0600

    Add files via upload

--- postFRET_V4/S6_results.m
+++ postFRET_V4/S6_results.m
@@ -49,10 +49,37 @@ rs = std(rateHistory(:, :, 4:end), 0, 3)*sqrt(config.repeatTime)
 
 
 figure; plot(data(:,1), data(:,5)); hold on; plot(DataBin(:,1), DataBin(:,4)+ 1); title('lower: norm. raw data; upper: simulated data');
+jcPlotStyle
 
-[Nr,Xr] = hist(data(:,5),100);
-[Ns,Xs] = hist(DataBin(:,4),100);
-figure;  plot(Xr, Nr/max(Nr)); hold on; 
-plot(Xs, ones(length(Xs),1)); plot(Xs, Ns/max(Ns)+1); title('lower raw data, upper simulated data FRET histogram');
+rhistDA = config.rhistDA;
+rhistFRET = config.rhistFRET;
 
+figure; 
+hr = round((max(data(:,2))-min(data(:,2)))/rhistDA);
+subplot(2, 3, 1), hist(data(:,2),hr); title('donor exp.');  jcPlotStyle
+   axis([min(Donor(:,1)-3*Donor(:,3)) max(Donor(:,1)+3*Donor(:,3)) 0 inf]); 
+hold on;
+
+hr = round((max(data(:,3))-min(data(:,3)))/rhistDA);
+subplot(2, 3, 2), hist(data(:,3),hr); jcPlotStyle
+  title('acceptor exp.'); axis([min(Accpt(:,1)-3*Accpt(:,3)) max(Accpt(:,1)+3*Accpt(:,3)) 0 inf]);
+
+hr = round((max(data(:,5))-min(data(:,5)))/rhistFRET); 
+  subplot(2, 3, 3), hist(data(:,5), hr); jcPlotStyle
+  title('FRET exp.'); axis([-0.1 1.1 0 inf]);
+
+trajD = DataBin(:,2);
+hr = round((max(trajD)-min(trajD))/rhistDA);
+subplot(2, 3, 4), hist(trajD, hr); jcPlotStyle
+   title('donor simu.'); axis([min(Donor(:,1)-3*Donor(:,3)) max(Donor(:,1)+3*Donor(:,3)) 0 inf]);
+
+trajA = DataBin(:,3);
+hr = round((max(trajA)-min(trajA))/rhistDA);
+subplot(2, 3, 5), hist(trajA, hr); jcPlotStyle
+   title('acceptor simu.'); axis([min(Accpt(:,1)-3*Accpt(:,3)) max(Accpt(:,1)+3*Accpt(:,3)) 0 inf]);
+
+trajFRET = DataBin(:,4);
+hr = round((max(trajFRET)-min(trajFRET))/rhistFRET); 
+subplot(2, 3, 6), hist(trajFRET, hr); jcPlotStyle
+   title('FRET simu.'); axis([-0.1 1.1 0 inf]);
 
